e28a2212a12822ef5cd9934a48724518f9e311e8
Ansh Sharma <akgccansh@gmail.com>
2026-06-27 23:17:33 +0530

    Cleared All ERRORS

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // FIREBASE APP
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
@@ -30,37 +29,4 @@ const db = getFirestore(app);
 
 const auth = getAuth(app);
 
-=======
-// FIREBASE APP
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-
-// FIRESTORE
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-// AUTH
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-const firebaseConfig = {
-
-  apiKey: "AIzaSyChrJuL-CT1490jYVQ7IBtrp2fDdGTVP-M",
-
-  authDomain: "web-app-a4752.firebaseapp.com",
-
-  projectId: "web-app-a4752",
-
-  storageBucket: "web-app-a4752.firebasestorage.app",
-
-  messagingSenderId: "244714993973",
-
-  appId: "1:244714993973:web:f167fb9a7555052fcfb47c"
-
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-const auth = getAuth(app);
-
->>>>>>> 893638e432ef6f1664e39e332b9bd919fc4174e3
 export { db, auth };
